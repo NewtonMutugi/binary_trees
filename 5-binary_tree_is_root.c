d0085846c65ca1a694cb2db7457a73725e0031f6
NewtonMutugi <newtonmut197@gmail.com>
2023-06-01 18:17:22 +0300

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,11 +9,7 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (!(node) || node->parent)
 		return (0);
-
-	if ((node->parent == NULL) && (node->left != NULL || node->right != NULL))
-		return (1);
-
-	return (0);
+	return (1);
 }
